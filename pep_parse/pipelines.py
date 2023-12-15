b8bf29a9c499464eac0b4ece9f8e5efdfba8816e
Aleksei Romanov <alekseivladyka@gmail.com>
2023-12-16 01:49:58 +0300

    First review

---
+++ pep_parse/pipelines.py
@@ -0,0 +1,38 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import time
+from pathlib import Path
+
+BASE_DIR = Path(__file__).absolute().parent.parent
+RESULTS_DIR = 'results'
+TIME_FORMAT = r'%Y-%m-%dT%H-%M-%S'
+FILENAME = 'status_summary_{}.csv'.format(time.strftime(TIME_FORMAT))
+
+
+class PepParsePipeline:
+    def open_spider(self, spider):
+        """При запуске паука создается словарь со статусами PEP"""
+        self.results = BASE_DIR / RESULTS_DIR
+        self.results.mkdir(exist_ok=True)
+        self.count_dir = dict()
+        self.total = 0
+
+    def process_item(self, item, spider):
+        """В процессе переборки item добавляем статусы pep в словарь,
+          и считаем их количество"""
+        status_quantity = self.count_dir.get(item['status']) or 0
+        self.count_dir[item['status']] = status_quantity + 1
+        return item
+
+    def close_spider(self, spider):
+        """При закрытии паука задаем формат вывода данных"""
+        with open(self.results / FILENAME, mode='w', encoding='utf-8') as f:
+            f.write('Статус,Количество\n')
+            self.count_dir['Total'] = sum(self.count_dir.values())
+            for status in self.count_dir.keys():
+                f.write(f'{status},{self.count_dir[status]}\n')
---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,31 @@
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['https://peps.python.org/']
+
+    def parse(self, response):
+        """Собирает информацию об URL-адресах всех PEP"""
+        pep_list = response.css(
+            '#numerical-index table.pep-zero-table tbody tr'
+        )
+        for pep in pep_list:
+            pep_link = pep.css('a::attr(href)').get()
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        """Собирает информацию со страницы конкретного PEP"""
+        h1 = response.css('#pep-content > h1::text').get().split()
+        number = h1[1]
+        name = ' '.join(h1[3:])
+        status = response.css('abbr::text').get()
+        data = {
+            'number': number,
+            'name': name,
+            'status': status,
+        }
+        yield PepParseItem(data)
